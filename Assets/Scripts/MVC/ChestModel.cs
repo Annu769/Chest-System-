74f518c74169589858c6bafb389ad1230cbed54e
Annu769 <109024549+Annu769@users.noreply.github.com>
2023-09-06 15:13:46 +0530

    Add ScriptableObject And Generic Single Ton

--- Assets/Scripts/MVC/ChestModel.cs
+++ Assets/Scripts/MVC/ChestModel.cs
@@ -1,18 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class ChestModel : MonoBehaviour
+namespace ChestSystem
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ChestModel
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
